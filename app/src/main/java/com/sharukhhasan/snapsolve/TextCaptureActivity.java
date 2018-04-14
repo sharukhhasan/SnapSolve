24a23e343907f8c0f5aa30689eb2847d7b6d1f71
Sharukh Hasan <sharukh-hasan@uiowa.edu>
2018-04-14 12:39:43 -0500

    completed TextCaptureActivity layout - v1

---
+++ app/src/main/java/com/sharukhhasan/snapsolve/TextCaptureActivity.java
@@ -0,0 +1,18 @@
+package com.sharukhhasan.snapsolve;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Created by Sharukh Hasan on 4/14/18.
+ * Copyright © 2018 Sharukh Hasan. All rights reserved.
+ */
+public class TextCaptureActivity extends AppCompatActivity {
+    private static final String TAG = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text_capture);
+    }
+}
