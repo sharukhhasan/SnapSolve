186f05d8ca23951c48063d086187d7c36aff4584
Sharukh Hasan <sharukh-hasan@uiowa.edu>
2018-04-14 13:25:34 -0500

    added methods to TextCaptureActivity to pause, stop, and release camera resources

---
+++ app/src/main/java/com/sharukhhasan/snapsolve/AppController.java
@@ -0,0 +1,20 @@
+package com.sharukhhasan.snapsolve;
+
+import android.app.Application;
+
+/**
+ * Created by Sharukh Hasan on 4/14/18.
+ * Copyright © 2018 Sharukh Hasan. All rights reserved.
+ */
+public class AppController extends Application {
+    private static final String TAG = "AppController";
+
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+
+    }
+}
